ba559178c51c42931867012b1406b096e94d7b54
Copser <iamrmot@gmail.com>
2015-10-09 07:02:15 +0200

    tests moved to the tests folder in root

--- django_ecommerce/contact/tests.py
+++ django_ecommerce/contact/tests.py
@@ -5,18 +5,18 @@ when you run "manage.py test".
 Replace this with more appropriate tests for your application.
 """
 
-from django.test import TestCase, SimpleTestCase
-from contact.views import ContactView
+# from django.test import TestCase, SimpleTestCase
+# from contact.views import ContactView
 
 
-class ContactViewTests(SimpleTestCase):
+# class ContactViewTests(SimpleTestCase):
 
-    """Docstring for ContactViewTests. """
-    def test_displayed_fields(self):
-        """TODO: Docstring for test_displayed_fields.
-        :returns: We are testing are ContactView to
-        see if we are returning the right fields.
+#    """Docstring for ContactViewTests. """
+#    def test_displayed_fields(self):
+#        """TODO: Docstring for test_displayed_fields.
+#        :returns: We are testing are ContactView to
+#        see if we are returning the right fields.
 
-        """
-        expected_fields = ['name', 'email', 'topic', 'message']
-        self.assertEquals(ContactView.Meta.fields, expected_fields)
+#        """
+#        expected_fields = ['name', 'email', 'topic', 'message']
+#        self.assertEquals(ContactView.Meta.fields, expected_fields)
